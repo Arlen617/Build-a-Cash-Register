41aa8992bd6204ed6994d37227b0f5fa2bf8a305
Arlen Nighshade <alkdfalkdjflka@gmail.com>
2024-10-01 16:59:53 -0600

    3

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-let price = 19.5;
+let price = 3.26;
 let cid = [
-  ["PENNY", 0.5],
-  ["NICKEL", 0],
-  ["DIME", 0],
-  ["QUARTER", 0],
-  ["ONE", 0],
-  ["FIVE", 0],
-  ["TEN", 0],
-  ["TWENTY", 0],
-  ["ONE HUNDRED", 0],
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
 const input = document.getElementById("cash");
 const output = document.getElementById("change-due");
 const button = document.getElementById("purchase-btn");
-let status = "OPEN";
-let sufficientFunds = true;
 
-const stringToMoney = (string) => {
+const stringToCents = (string) => {
   const reference = {
-    PENNY: 0.01,
-    NICKEL: 0.05,
-    DIME: 0.1,
-    QUARTER: 0.25,
-    ONE: 1,
-    FIVE: 5,
-    TEN: 10,
-    TWENTY: 20,
-    "ONE HUNDRED": 100,
+    PENNY: 1,
+    NICKEL: 5,
+    DIME: 10,
+    QUARTER: 25,
+    ONE: 100,
+    FIVE: 500,
+    TEN: 1000,
+    TWENTY: 2000,
+    "ONE HUNDRED": 10000,
   };
   return reference[string];
 };
-
-function roundTohundredth(num) {
-  return Math.round(num * 100) / 100;
-}
 
 function checkStatus() {
   let change = parseFloat(input.value) - price;
@@ -45,32 +39,37 @@ function checkStatus() {
 }
 
 function calculateChange(cash) {
-  let change = cash - price;
-  const changeUnitsObj = {};
+  let change = (cash - price) * 100;
+  let changeUnitsObj = {};
 
-  cid.sort((a, b) => stringToMoney(b[0]) - stringToMoney(a[0]));
+  cid.sort((a, b) => stringToCents(b[0]) - stringToCents(a[0]));
+
+  cid = cid.map((arr) => [arr[0], arr[1] * 100]);
   cid.forEach((pair) => {
-    while (change >= stringToMoney(pair[0]) && pair[1] > 0) {
-      pair[1] -= stringToMoney(pair[0]);
-      change -= stringToMoney(pair[0]);
-      change = roundTohundredth(change);
+    while (change >= stringToCents(pair[0]) && pair[1] > 0) {
+      pair[1] -= stringToCents(pair[0]);
+      change -= stringToCents(pair[0]);
       if (changeUnitsObj[pair[0]]) {
-        changeUnitsObj[pair[0]] += stringToMoney(pair[0]);
+        changeUnitsObj[pair[0]] += stringToCents(pair[0]);
       } else {
-        changeUnitsObj[pair[0]] = stringToMoney(pair[0]);
+        changeUnitsObj[pair[0]] = stringToCents(pair[0]);
       }
-      changeUnitsObj[pair[0]] = roundTohundredth(changeUnitsObj[pair[0]]);
     }
   });
   if (change > 0) {
     return null;
   }
+
+  for (let key in changeUnitsObj) {
+    changeUnitsObj[key] = changeUnitsObj[key] / 100;
+  }
+
   return changeUnitsObj;
 }
 
 function createMsg() {
   let message = `Status: ${checkStatus()}`;
-  const changeObj = calculateChange(input.value);
+  const changeObj = calculateChange(parseFloat(input.value));
   if (parseFloat(input.value) === price) {
     return "No change due - customer paid with exact cash";
   }
